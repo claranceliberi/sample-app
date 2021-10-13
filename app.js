1fca9ec586fcca982bec9a47ec737d6bfa21d221
claranceliberi <liberintwari@gmail.com>
2021-10-13 08:14:53 +0200

    feat: initials

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ import { createServer } from 'http'
 import { createApp } from 'h3'
 
 const app = createApp()
-app.use('/', () => 'Hello world!')
+
+app.use('/req', (req) => ({...req.url}))
+
+app.use('/', () => '<h1>Hello world!</h1>')
+
+
 
 createServer(app).listen({port:process.env.PORT || 3000},() => {
     console.log('server started')
